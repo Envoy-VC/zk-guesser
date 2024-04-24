252417a44532fb1af2a8bb0eeefb42926ee19800
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-24 20:05:08 +0530

    chore: add coordinates and oracle for get distance

--- apps/www/index.js
+++ apps/www/index.js
@@ -1,6 +1,41 @@
-import { hashMessage, toBytes } from 'viem';
+import {
+  createWalletClient,
+  hashMessage,
+  http,
+  recoverPublicKey,
+  toBytes,
+} from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { mainnet } from 'viem/chains';
 
-const hash = hashMessage('1-4');
-console.log(hash);
+const account = privateKeyToAccount(
+  '0x91487f27e131659af251cad31eb46012d83410428c1ddf8392611c40fccd91bc'
+);
 
-// [72, 124, 60, 126, 19, 237, 122, 212, 246, 164, 136, 133, 94, 136, 206, 195, 54, 101, 100, 167, 26, 34, 9, 28, 24, 146, 25, 235, 220, 36, 22, 113]
+const client = createWalletClient({
+  account,
+  chain: mainnet,
+  transport: http(),
+});
+
+console.log('Address: ', client.account.address);
+const message = 'hello world';
+console.log('Hashed Message: ', hashMessage(message, 'bytes'));
+
+const sig = await client.signMessage({
+  message: { raw: toBytes(message) },
+});
+
+console.log('Signature: ', toBytes(sig).subarray(0, 64));
+
+const pubKey = await recoverPublicKey({
+  hash: hashMessage(message),
+  signature: sig,
+});
+
+const pub = pubKey.slice(4);
+console.log('Public Key: ', pub);
+
+const pub_x = toBytes(`0x${pub.substring(0, 64)}`);
+const pub_y = toBytes(`0x${pub.substring(64)}`);
+console.log({ pub_x, pub_y });
