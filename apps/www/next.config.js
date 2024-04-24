3b088a50353a014199b3721ab839874afc47e095
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-24 22:39:12 +0530

    chore: remove next-rpc

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,8 +1,6 @@
-import withRpc from 'next-rpc';
-
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default withRpc()(config);
+export default config;
