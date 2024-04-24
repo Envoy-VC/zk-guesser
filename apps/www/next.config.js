710f092f61d591b4aec6549d4507a49418a301cb
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-24 16:26:38 +0530

    chore: add ecrecover lib and tests

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,10 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+import withRpc from 'next-rpc';
+
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default withRpc()(config);
