c38609e32d8312450c9a130eb40525832e3885e3
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-25 19:58:43 +0530

    chore: update home page for responsiveness

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -4,13 +4,15 @@ import Buildings from '~/screens/Buildings';
 
 const Home = () => {
   return (
-    <div className='relative h-screen'>
-      <div className='absolute top-0 z-[100] h-full w-full  text-white'>
-        <div className='mx-auto max-w-screen-xl py-24'>
-          <div className='font-pricedown text-[5rem]'>MAPPING OUT ZK</div>
+    <div className='relative h-screen overflow-hidden'>
+      <div className='absolute top-0 h-full w-full'>
+        <Buildings />
+      </div>
+      <div className='relative z-[10000] mx-4 my-12 w-fit text-white sm:mx-24 sm:my-24 sm:px-0'>
+        <div className='font-pricedown text-[3.2rem] md:text-[5rem]'>
+          MAPPING OUT ZK
         </div>
       </div>
-      <Buildings />
     </div>
   );
 };
