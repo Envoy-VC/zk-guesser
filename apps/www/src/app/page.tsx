66640a1d4806a35bbe5408c268c2bf1ab03a2b0d
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-26 23:58:04 +0530

    chore: uncomment background

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <div className='relative h-screen overflow-hidden'>
       <div className='absolute top-0 h-full w-full bg-black/75'>
-        {/* <Buildings /> */}
+        <Buildings />
       </div>
       <div className='relative z-[100] mx-4 my-12 flex w-fit flex-col gap-12  text-white sm:mx-24 sm:my-24 sm:px-0'>
         <div className='font-pricedown text-[3.2rem] md:text-[5rem] '>
