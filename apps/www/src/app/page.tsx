0f4fe70dac8471f64e89d828abde045cb4da93e8
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-26 11:35:41 +0530

    chore: generate targets

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -12,14 +12,14 @@ const Home = () => {
 
   return (
     <div className='relative h-screen overflow-hidden'>
-      <div className='absolute top-0 h-full w-full'>
-        <Buildings />
+      <div className='absolute top-0 h-full w-full bg-black/75'>
+        {/* <Buildings /> */}
       </div>
       <div className='relative z-[100] mx-4 my-12 flex w-fit flex-col gap-12  text-white sm:mx-24 sm:my-24 sm:px-0'>
         <div className='font-pricedown text-[3.2rem] md:text-[5rem] '>
           MAPPING OUT ZK
         </div>
-        <div className='relative min-w-[80vw] w-full'>
+        <div className='relative w-full min-w-[80vw]'>
           {isOpen ? <GameMenu /> : <HomeNavigation />}
         </div>
       </div>
